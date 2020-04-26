501f8663c0460b83a591bd21b04681dae67f3667
ishaq1399 <ishaqhusainynur99@gmail.com>
2020-04-26 14:07:28 +0700

    s

---
+++ app/src/main/java/com/arvita/crudvolley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.arvita.crudvolley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.125/ci/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.43.125/ci/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.43.125/ci/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE = "http://192.168.43.125/ci/Mahasiswa/ApiUpdate";
+}
